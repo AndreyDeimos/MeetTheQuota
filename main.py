5f9b9aab1a58a38c3aeb62bd4a5af0b99d92ed52
Deimos <82042334+AndreyDeimos@users.noreply.github.com>
2024-03-13 01:12:19 +0300

    ended

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import pygame
 import sys
 from random import choice
 from lib.dynamyc import find_cheapest_path, generate_random_table
+
 pygame.init()
 size = 1920, 1080
 
@@ -16,25 +17,37 @@ if sys.platform == 'win32':
     except AttributeError:
         pass  # Windows XP doesn't support monitor scaling, so just do nothing.
 
+# fps limit to 60
 fps = 60
 clock = pygame.time.Clock()
+
+# making the window
 screen = pygame.display.set_mode(size, pygame.FULLSCREEN, pygame.HWSURFACE)
 pygame.display.set_caption("Meet the quota")
 running = True
 
+# starting values
 table_size = 3
 spread = 2
 table = generate_random_table(table_size, spread)
+
+# setting up the table
 table_surface = pygame.Surface((1000, 1000), pygame.HWSURFACE)
+table_lines_surface = pygame.Surface((1000, 1000), pygame.HWSURFACE)
+table_numbers_surface = pygame.Surface((1000, 1000), pygame.HWSURFACE)
+
+# variables for player state
 player_was = []
 player_x = 0
 player_y = table_size - 1
-pressed_right = 0
-pressed_up = 0
-table_lines_surface = pygame.Surface((1000, 1000), pygame.HWSURFACE)
-look_how_bad_are_you = 0
 sumscore = 0
 
+# handlers for input once per frame
+pressed_right = 0
+pressed_up = 0
+return_pressed = False
+
+# class for cleaner text implementation, to change font or size put it first before set text
 class Text:
     def __init__(self):
         self.font = pygame.font.SysFont('dubai', 72)
@@ -57,49 +70,55 @@ class Text:
         screen.blit(self.surface, self.text_rect)
 
 
-return_pressed = False
-
 # return to tutorial when release
-gamestate = "game"
+gamestate = "tutorial"
 
+# state for button choice
 button_state = 1
 
+# Main menu game name
 Welcome_text = Text()
 Welcome_text.set_font("dubai", 120)
 Welcome_text.set_text("Meet the quota", "red")
 
+# tutorial of movement in the start
 tutorial_text = Text()
 tutorial_text.set_text("arrows to move, enter to select", "red")
 tutorial_text.set_cords(960, 540)
 
+# press enter to continue text
 continue_text = Text()
 continue_text.set_font("dubai", 36)
 continue_text.set_text("Press enter to continue", "red")
 continue_text.set_cords(960, 945)
 
+# button for starting a game
 play_button = pygame.Surface((350, 100))
 play_button.fill("red")
 play_text = Text()
 play_text.set_text("play", "black")
 play_text.set_cords(175, 50)
 
+# button for exiting the game
 exit_button = pygame.Surface((350, 100))
 exit_button.fill("red")
 exit_text = Text()
 exit_text.set_cords(240, 50)
 exit_text.set_text("exit", "black")
 
+# variable for state of pre game tutorial (slides)
 game_tutorial_state = 1
 
-
+# rendering the slides of pregame tutorial
 def render_game_tutorial():
     global game_tutorial_state, gamestate
-    if game_tutorial_state == 2:
+    if game_tutorial_state == 4:
         gamestate = "game"
-    elif game_tutorial_state == 1:
-        continue_text.render_text(screen)
+    else:
+        img = pygame.image.load(f"static/slide{game_tutorial_state}.png")
+        screen.blit(img, (0,0))
 
-
+# rendering the play button
 def render_play_button():
     global button_state
     if button_state == 1:
@@ -109,7 +128,7 @@ def render_play_button():
     play_text.render_text(play_button)
     screen.blit(play_button, (640 - 175, 810 - 50))
 
-
+# rendering the exit button
 def render_exit_button():
     global button_state
     if button_state == 1:
@@ -119,8 +138,9 @@ def render_exit_button():
     exit_text.render_text(exit_button)
     screen.blit(exit_button, (1280 - 175, 810 - 50))
 
+# rendering the table (separately for optimization)
 def table_lines():
-    global table_size, table_lines_surface
+    global table_size, table_lines_surface, table_numbers_surface
     table_lines_surface.fill("black")
     pygame.draw.line(table_lines_surface, "red", (0, 0), (999, 0), width=1)
     pygame.draw.line(table_lines_surface, "red", (999, 0), (999, 999), width=1)
@@ -132,27 +152,28 @@ def table_lines():
                          (999, pos * i), width=1)
     for i in range(table_size):
         pygame.draw.line(table_lines_surface, "red", (pos * i, 0), (pos * i, 999), width=1)
-
-def table_numbers():
-    pos = 1000 // table_size
     for i in range(table_size):
         for j in range(table_size):
-            if (i, j) not in player_was:
-                render_number(table[j][i], pos * (i + 0.5), pos * (j + 0.5), pos - 1)
+            render_number(table[j][i], pos * (i + 0.5), pos * (j + 0.5), pos - 1)
+
+# displaying the rendered table
 def table_render():
-    global table_size, table, table_surface, table_lines_surface
+    global table_size, table, table_surface, table_lines_surface, table_numbers_surface
     pos = 1000 // table_size
     table_surface.fill("black")
     table_surface.blit(table_lines_surface, (0, 0))
+    for i, j in player_was:
+        pygame.draw.rect(table_surface, "black", [pos * i + 1, pos * j  + 1, pos - 1, pos - 1], width=0)
     pygame.draw.rect(table_surface, "red", [pos * player_x , pos * player_y, pos, pos], width=0)
 
 
 def render_number(number, x, y, size):
+    global table_lines_surface
     text = Text()
     text.set_font('dubai', size)
     text.set_text(str(number), "red")
     text.set_cords(x, y)
-    text.render_text(table_surface)
+    text.render_text(table_lines_surface)
 
 score = Text()
 level = Text()
@@ -163,6 +184,14 @@ quota.set_cords(1450, (1080 // 4) * 3)
 scorecounter = 0
 quotascore = 0
 
+# Timer variables
+timer_font = pygame.font.SysFont("dubai", 72)
+timer_color = "red"
+timer_x, timer_y = 1450, (1080 // 4) * 2
+timer_start = pygame.time.get_ticks()
+timer_duration = 15000  # 30 seconds
+remaining_time = 15000
+
 def render_game():
     global table_surface
     table_render()
@@ -172,11 +201,30 @@ def render_game():
     quota.render_text(screen)
     screen.blit(table_surface, (40, 40))
 
+def render_timer(): 
+    global timer_font, timer_color, timer_x, timer_y, timer_start, timer_duration, remaining_time
+    if gamestate == "game":
+        elapsed_time = pygame.time.get_ticks() - timer_start
+        remaining_time = max(timer_duration - elapsed_time, 0)
+        seconds = remaining_time // 1000
+        milliseconds = (remaining_time % 1000) // 10
+        timer_text = f"Time: {seconds:02d}:{milliseconds:02d}"
+        timer_surface = timer_font.render(timer_text, True, timer_color)
+        timer_x, timer_y = 1450 - timer_surface.get_width() // 2, (1080 // 4) * 2 - timer_surface.get_height() // 2
+        screen.blit(timer_surface, (timer_x, timer_y))
+
+end_text = Text()
+end_text.set_font("dubai", 72)
+def render_end_screen():
+    end_text.set_text(f"Your level was: {spread + table_size - 4}", "red")
+    end_text.set_cords(960, 540)
+    end_text.render_text(screen)
+    continue_text.render_text(screen)
+
 
 def render_tutorial():
     tutorial_text.render_text(screen)
     continue_text.render_text(screen)
-
 
 def render_start_menu():
     text_x = size[0] // 2
@@ -185,7 +233,6 @@ def render_start_menu():
     Welcome_text.render_text(screen)
     render_play_button()
     render_exit_button()
-
 
 def handle_event(event):
     global gamestate, button_state, running, game_tutorial_state, return_pressed, player_y, player_x, player_was, pressed_right, pressed_up, scorecounter
@@ -227,7 +274,10 @@ def handle_event(event):
                     player_y -= 1
                     scorecounter += table[player_x][player_y]
                     print("pressed up")
-
+    if gamestate == "endscreen":
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_RETURN and not return_pressed:
+                gamestate = "start_menu"
 def new_table():
     global player_x, player_y, player_was, table, table_size, spread, scorecounter
     harder = choice((1, 2))
@@ -243,23 +293,32 @@ def new_table():
     scorecounter = 0
 
 def update_scene():
-    global player_x, player_y, table_size, gamestate, quotascore, sumscore, table, table_size, spread, scorecounter
+    global player_x, player_y, table_size, gamestate, quotascore, sumscore, table, table_size, spread, scorecounter, timer_start, remaining_time
     if player_x == table_size - 1 and player_y == 0:
-        if look_how_bad_are_you == 0:
-            ...
         # adding the last value player stepped on since no after adding is called due no inputs (look game gamestate handling)
         scorecounter += table[0][table_size - 1]
         cheapest_path = find_cheapest_path(table)
         sumscore += int(100 * (cheapest_path / scorecounter))
         print(cheapest_path, scorecounter)
-        quotascore += 50 * ((spread + table_size - 3) / 20 + 1)
+        quotascore += 50 + int((spread + table_size - 3) / 10) * 25
         if quotascore > sumscore:
-            gamestate = 'start_menu'
+            gamestate = 'endscreen'
             sumscore = 0
             quotascore = 0
             table_size = 2
             spread = 2
         new_table()
+        timer_start = pygame.time.get_ticks()
+        remaining_time = 15000
+    elif remaining_time == 0:
+        gamestate = 'endscreen'
+        sumscore = 0
+        quotascore = 0
+        table_size = 2
+        spread = 2
+        new_table()
+        remaining_time = 15000
+        timer_start = pygame.time.get_ticks()
 def draw_scene():
     global gamestate
     screen.fill("black")
@@ -271,6 +330,9 @@ def draw_scene():
         render_game_tutorial()
     if gamestate == "game":
         render_game()
+        render_timer()
+    if gamestate == "endscreen":
+        render_end_screen()
 
 table_lines()
 
@@ -284,8 +346,8 @@ while running:
         handle_event(event)
     update_scene()
     draw_scene()
-    # print(gamestate)
     pygame.display.flip()
     clock.tick(fps)
+
 pygame.quit()
 sys.exit()
